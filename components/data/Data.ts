02a494f74efb201ad3e5a99e0443ac4d41d49998
titvvn <chuongtoan1602@gmail.com>
2025-06-13 13:47:58 +0700

    Update

--- components/data/Data.ts
+++ components/data/Data.ts
@@ -5,4 +5,23 @@ export const dataReview = [
           url: 'https://www.youtube.com/embed/hfaBK7YD8ZA',
           color: "#00D768",
      },
+     {
+          id: 1,
+          title: 'GIỚI THIỆU THÀNH VIÊN',
+          url: 'https://www.youtube.com/embed/-Xs1RvPc8-8',
+          color: "#00D768",
+     },
+     {
+          id: 1,
+          title: 'GIỚI THIỆU THÀNH VIÊN',
+          url: 'https://www.youtube.com/embed/3IQJ5IlRmgE',
+          color: "#00D768",
+     },
+     {
+          id: 1,
+          title: 'GIỚI THIỆU THÀNH VIÊN',
+          url: 'https://www.youtube.com/embed/e4zEzv7LNQA',
+          color: "#00D768",
+     },
 ];
+
